2cdf11b737bcd3b7963ab35221f8cb8604b88243
youjin <w2117@e-mirim.hs.kr>
2023-05-22 12:10:57 +0900

    template 엔진

--- src/main/java/study/thymeleaf/MyController.java
+++ src/main/java/study/thymeleaf/MyController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MyController {
@@ -39,5 +43,33 @@ public class MyController {
 
         // ModelAndView 객체 반환
         return mav;
+    }
+    @GetMapping("/template3")
+    public String template3(Model m) {
+        // template 엔진에서 condition이의 값이 true 일 때,
+        // template3.html에서 condition이 true이면 실행되게 하는 것
+        // th:if="${condition1} == true"
+        m.addAttribute("condition1", true);
+        m.addAttribute("condition2", false);
+
+        Book book = new Book("김철수", "부자되는 방법", 10000);
+        book.setSale(true);
+        book.setSaleAmount(0.3);
+        //<div th:object="${book}"> template.html에서 book 접근
+        m.addAttribute("book", book);
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book("김철수", "부자되는 방법 1", 10000));
+        bookList.add(new Book("김철수", "부자되는 방법 2", 20000, true, 0.2));
+        bookList.add(new Book("김철수", "부자되는 방법 3", 30000));
+        bookList.add(new Book("김철수", "부자되는 방법 4", 40000, true, 0.3));
+        m.addAttribute("book_list", bookList);
+        Map<String, Book> bookMap = new HashMap<>();
+        bookMap.put("book1", new Book("김철수", "부자되는 방법 1", 10000));
+        bookMap.put("book2", new Book("김철수", "부자되는 방법 2", 20000, true, 0.2));
+        bookMap.put("book3", new Book("김철수", "부자되는 방법 3", 30000));
+        bookMap.put("book4", new Book("김철수", "부자되는 방법 4", 40000, true, 0.3));
+        m.addAttribute("book_map", bookMap);
+        m.addAttribute("grade", "A");
+        return "template3";
     }
 }
